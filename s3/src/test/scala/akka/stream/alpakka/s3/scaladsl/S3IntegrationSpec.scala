895913193daefb7d7c41f8c650a9ac1a519f6f88
sfali <sali@loyalty.com>
2018-06-18 07:13:01 -0400

    running integration tests, #938    * reverting back main docker-compose.yml    * run s3/docker-compose.yaml with -f option    * update endpointUrl

--- s3/src/test/scala/akka/stream/alpakka/s3/scaladsl/S3IntegrationSpec.scala
+++ s3/src/test/scala/akka/stream/alpakka/s3/scaladsl/S3IntegrationSpec.scala
@@ -350,8 +350,7 @@ class AWSS3IntegrationSpec extends S3IntegrationSpec
 class MinioS3IntegrationSpec extends S3IntegrationSpec {
   val accessKey = "TESTKEY"
   val secret = "TESTSECRET"
-  // TODO: find a way to pass endpoint url, instead of hard code here
-  val endpointUrl = "http://localhost:9000"
+  val endpointUrl = "http://localhost:9001"
 
   val staticProvider = new AWSStaticCredentialsProvider(
     new BasicAWSCredentials(accessKey, secret)
